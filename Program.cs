9e19fc95bad2017e9e16a34dc68e97b6c96f1c1e
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2022-01-21 17:39:03 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print your expression:");
+            string expression = Console.ReadLine();
+
+            Queue<char> numbers = new Queue<char>();
+            Queue<char> operators = new Queue<char>();
+
+            
+        }
+    }
