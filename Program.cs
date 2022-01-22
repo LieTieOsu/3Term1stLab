0c537b7b4cb688a85f8bb1060fc977a658cd52b2
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2022-01-22 21:29:24 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,3 +10,64 @@ var postfixTokens = ShuntingYard(tokens);
 var result = Calculate(postfixTokens); 
 Console.Write("<"); 
 Console.WriteLine(result);
+
+Queue Tokenizer(string expression)
+{
+    var tokenized = new Queue();
+    var num = new Queue();
+  
+  
+  
+  
+   return tokenized;
+}
+
+
+Queue ShuntingYard(Queue tokenized)
+{
+    var operatorStack = new Stack();
+    var output = new Queue();
+  
+  
+  return output;
+}
+
+String Calculate(Queue postfixTokenQueue)
+{
+    var set = new Stack();
+String Calculate(Queue postfixTokenQueue)
+{
+    var set = new Stack();
+  
+  
+  
+  
+  return result;
+}
+  
+  tring Count(string firstNum, string secondNum, string element)
+{ 
+    double result;
+    switch (element)
+    {
+        case "+":
+            result = double.Parse(firstNum) + double.Parse(secondNum);
+            break;
+        case "-":
+            result = double.Parse(firstNum) - double.Parse(secondNum);
+            break;
+        case "/":
+            result = double.Parse(firstNum) / double.Parse(secondNum);
+            break;
+        case "*":
+            result = double.Parse(firstNum) * double.Parse(secondNum);
+            break;
+        case "x":
+            result = double.Parse(firstNum) * double.Parse(secondNum);
+            break;
+        default:
+            throw new Exception("Error, use only +,-,/,*(x)");
+    }
+
+    return result.ToString();
+} 
