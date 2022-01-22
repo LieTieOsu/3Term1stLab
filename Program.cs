d57760621fe0b92c8f5af3abb317fe9de1c5053d
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2022-01-22 21:25:49 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
+using System.Globalization;
+using Stack = Task.Stack;
+using Queue = Task.Queue;
 
+
+Console.Write(">");
+var input = Console.ReadLine();
+var tokens = Tokenizer(input);
+var postfixTokens = ShuntingYard(tokens);
+var result = Calculate(postfixTokens); 
+Console.Write("<"); 
+Console.WriteLine(result);
