f4cae60ef6bcbe7330df21994a10f33832175c4c
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2022-01-22 21:24:09 +0200

    Update Queue.cs

--- Queue.cs
+++ Queue.cs
@@ -7,3 +7,15 @@ public class Queue
     private string[] array = new string[Capacity];
 
     private int _pointer;
+    
+    public void Enqueue(string value)
+    {
+        if (_pointer == array.Length)
+        {
+            throw new Exception("Stack overflowed");
+        }
+
+        array[_pointer] = value;
+        _pointer++;
+    }
+    
